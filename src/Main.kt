b187f200cfc65e667cc95c0396d8f12ed3cb5380
TakaDevCode <101286474+TakaDevCode@users.noreply.github.com>
2026-01-12 15:28:40 -0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    //Variables mutables e inmutables
+    //Declaracion de variables mutables e inmutables
+    var nombre = "Jose"
+    val edad = 30
+    println("Hola, mi nombre es $nombre y tengo $edad años.")
+    //Uso de funcionalidades de String
+    println("Nombre tiene ${nombre.length} caracteres.")
+}
